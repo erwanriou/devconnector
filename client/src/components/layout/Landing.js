70f65d62c36953e5226284eed7516e3a4c149b87
Erwan Riou <rr.1.riou@gmail.com>
2018-07-30 16:17:36 +0200

    [project] - set up landing page component styling

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,8 +3,18 @@ import React from 'react'
 class Landing extends React.Component {
   render() {
     return (
-      <div className='landing '>
-        <p>LANDING PAGE</p>
+      <div className='landing'>
+        <div className="dark">
+          <div className='container'>
+            <h1>Developer Connector</h1>
+            <h2>Create a developer profile/portfolio, share posts and get help from other developers</h2>
+            <div className="register">
+              <button className='greenbtn'>Sign Up</button>
+              <button className='whitebtn'>Login</button>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
