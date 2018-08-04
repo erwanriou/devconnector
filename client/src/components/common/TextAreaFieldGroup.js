be81b5872979f7e0afc3f3ae8b9052f99a7dba9e
erwan riou <rr.1.riou@gmail.com>
2018-08-04 18:28:59 +0200

    [project] - Adding textareafield template

--- client/src/components/common/TextAreaFieldGroup.js
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -6,30 +6,25 @@ import * as FontAwesome from 'react-icons/fa'
 
 import Card from  '../auth/Card'
 
-const TextFieldGroup = ({
+const TextAreaFieldGroup = ({
   name,
   placeholder,
   value,
-  label,
   error,
-  type,
   onChange,
-  disabled,
-  title
+  title,
 }) => {
   return (
     <Fragment>
       <div className='field'>
-        <input
+        <textarea
           className={classnames('', {
             'invalid': error
           })}
-          type={type}
           name={name}
           value={value}
           onChange={onChange}
           placeholder={placeholder}
-          disabled={disabled}
         />
         <Popup
           trigger={<FontAwesome.FaQuestionCircle className='questionicon'/>}
@@ -48,13 +43,7 @@ TextFieldGroup.proptypes = {
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   error: PropTypes.string,
-  type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string,
 }
 
-TextFieldGroup.defaultProps = {
-  type: 'text'
-}
-
-export default TextFieldGroup
+export default TextAreaFieldGroup
