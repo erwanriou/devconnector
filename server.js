25e78999da9d012b1270ca08cb63bff4caca936c
Erwan Riou <rr.1.riou@gmail.com>
2018-08-17 19:38:46 +0200

    [PROJECT] -  Prepare for second deploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/posts', posts)
 
 // Server static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build')
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
